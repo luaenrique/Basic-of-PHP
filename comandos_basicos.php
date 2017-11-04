ed9d185788f6b834120cc64b90901653ab5d9d1b
Luã Enrique Zangrande <lua.enriquezan@gmail.com>
2017-11-04 14:39:10 -0200

    Create comandos_basicos.php

---
+++ comandos_basicos.php
@@ -0,0 +1,16 @@
+<?php
+    $var1 = 3;
+    $var2 = 4;
+    $morango = "Olá mundo!";
+    echo $var1 + $var2;
+    echo "<br>$morango";
+    //o comando acima emite mensagens na tela.
+    /*
+     echo "hello world!";
+    
+     isso é um comentário
+    
+    
+    */
+    #oi
+?>
